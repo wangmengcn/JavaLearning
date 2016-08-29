f2b3cd1fc4c527a3d61b8feb034b9c48018f98bb
wangmengcn <eclipse_sv@163.com>
2016-08-29 16:18:04 +0800

    moved to new floder

---
+++ websocket/src/main/java/com/eclipsesv/Application.java
@@ -0,0 +1,15 @@
+package com.eclipsesv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by eclipse on 16/8/23.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String... args){
+        SpringApplication.run(Application.class,args);
+    }
+
+}
